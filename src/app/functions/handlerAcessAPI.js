80f9f239d2da69d875562b0c4067e174e3caba17
gabriela <113440728+viniciussolique39@users.noreply.github.com>
2023-09-13 11:14:58 -0400

    Mudanças

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,21 +1,20 @@
 'use server'
 
 const usuarios = [
-    {name:"vinicius", email:"vinicius@gmail.com", password:"solique", token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"},
-    {name:"ingrid", email:"ingrid@gmail.com", password:"souza", token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"}
+    
+    {name:"vinicius",
+     email:"vinicius@gmail.com", 
+     password:"solique", 
+     token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    
+    }
 ]
 
-const getUserAuthenticated = (user) => {
-    let userAuth = {};
-    usuarios.find((e) => {
-        if(e.email === user.email && e.password === user.password){
-            userAuth = e;
-        };
-    })
-    return userAuth;
-}
-
-const getUsers = () =>{
-        return usuarios
-}
-export { getUsers, getUserAuthenticated };
+const getUserAuthenticated = (user) => { 
+    return usuarios.find(u => u.email === user.email && u.password === user.password);
+ }
+ 
+ const getUsers = () =>{
+       return usuarios
+ }
+ export { getUsers, getUserAuthenticated };
