aba1b3881bfc9dfd9a594a8de0ed0414a8ec6793
gabriela <113440728+viniciussolique39@users.noreply.github.com>
2023-09-16 14:39:12 -0400

    Senhor jesus

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 
+
 export default function Login() {
   const [user, setUser] = useState({
     email: '',
