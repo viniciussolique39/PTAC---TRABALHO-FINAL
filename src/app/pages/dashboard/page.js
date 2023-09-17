b3e700a36f5842b4a1ff43268bd32fe665eca52b
gabriela <113440728+viniciussolique39@users.noreply.github.com>
2023-09-16 22:39:18 -0400

    Estilização

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -3,16 +3,18 @@ import { getUsers } from "@/app/functions/handlerAcessAPI";
 export default async function Dashboard() {
    const users = await getUsers();
     return (
-        <div className="asus"> 
-            <h1>Dashboard - Listando Usuários</h1>
+        <div className="lista"> 
+        <fieldset>
+        <legend><b>Listando Usuários</b></legend>
+
         {users.map(user => (
             <div>
                 <h2>{user.name}</h2>
                 
                 <h2>{user.email}</h2>
             </div>
-        ))} 
-            
+        ))}
+        </fieldset>
         </div>
     );
 };
