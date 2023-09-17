1fb24a55311e2569773b450b9f92ee7464f75588
gabriela <113440728+viniciussolique39@users.noreply.github.com>
2023-09-16 21:46:03 -0400

    Colocando outras classes CSS nas outras páginas

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -3,8 +3,8 @@ import { getUsers } from "@/app/functions/handlerAcessAPI";
 export default async function Dashboard() {
    const users = await getUsers();
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className="asus"> 
+            <h1>Dashboard - Listando Usuários</h1>
         {users.map(user => (
             <div>
                 <h2>{user.name}</h2>
