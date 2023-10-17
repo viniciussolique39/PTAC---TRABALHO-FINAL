00535538875b2300c19b67631f514399389afaa6
Vinícius <vinicius.solique@estudante.ifms.edu.br>
2023-10-17 15:41:27 -0400

    Update no halderAcessAPI

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,27 +1,29 @@
 'use server'
 
-const usuarios = [
+//const usuarios = [
     
-    {name:"Vinícius de Souza Solique",
-     email:"vinicius@gmail.com", 
-     password:"solique", 
-     token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-     },
+    //{name:"Vinícius de Souza Solique",
+    // email:"vinicius@gmail.com", 
+     //password:"solique", 
+     //token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+     //},
 
-     {
-        name:"Marcelino Vitor",
-        email: "marcelino@ifms.edu.br",
-        senha: "vitor",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+     //{
+        //name:"Marcelino Vitor",
+        //email: "marcelino@ifms.edu.br",
+        //senha: "vitor",
+        //token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
-     }
-]
+     //}
+//]
+
+const url="https://aula-17-10-lemon.vercel.app";
 
 const getUserAuthenticated = (user) => { 
-    return usuarios.find(u => u.email === user.email && u.password === user.password);
+   
  }
  
  const getUsers = () =>{
-       return usuarios
+   
  }
  export { getUsers, getUserAuthenticated };
