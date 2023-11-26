6f2fd14d41a5f0d5e9e917ffb83c27939fcc5821
Vinícius <vinicius.solique@estudante.ifms.edu.br>
2023-11-26 15:33:11 -0400

    Update no register

--- src/app/pages/dashboard/register/page.js
+++ src/app/pages/dashboard/register/page.js
@@ -1,57 +1,60 @@
 'use client'
-import  React,{ useState } from "react";
+import { postUsers } from "@/app/functions/handlerAcessAPI";
+import React, { useState } from "react";
 import { useRouter } from 'next/navigation';
-import { postUser } from '@/app/functions/handlerAcessAPI';
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import {ToastContainer, toast } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css'; 
 
+export default function RegistrarUsuario() {
+    const [user, setUser] = useState ({
+        name: '',
+        email: '',
+        password: '',
+});
 
-export default function Register() {
-    const [user, setUser] = useState({
-      name: "",
-        email: "",
-        password: ""
+const {push} = useRouter();
+
+const handlerFormSubmit = async(event) =>{
+    event.preventDefault();
+    try{
+        await postUsers(user);
+        await new Promise((resolve) => {
+        toast.success ('Usuário cadastrado com sucesso!');
+        setTimeout(resolve, 5000);
       });
-  
-      const { push } = useRouter();
-      const handlerSubmit = async (event) => {
-        event.preventDefault();
+      return push("/pages/dashboard");
+  }catch{
+      return toast.error('erro');
+  }
+};
+return (
+  <div>
+      <h1 className='conta'>Registrar Usuário</h1>
+      <div className='container'>
+      <form className='contact_form' onSubmit={handlerFormSubmit}>
           
-          try {
-            await postUser(user);
-           /* await new Promise((resolve) => {
-              setTimeout(()=> resolve, 5000);
-            });*/
-            return push ("/pages/dashboard");
-            } catch {
-              return toast.error ("Erro");
-        
-            }
-      }
+          <div className='nome'> 
+          <input required placeholder='Digite seu nome' type="text" name="name" value={user.name}
+           onChange={(e) => { setUser({ ...user, name: e.target.value }) }}/>
+          </div>
 
-    return (
-         <div className='conteiner'>
-          <fieldset>
-            <legend><b>Registrar Usuários</b></legend>
-                <form method="post" onSubmit={handlerSubmit}>
-                <div className='inputBox'> 
-                <input id='text' placeholder='Digite seu nome' type="text" name="name"  className="inputUser" value={user.name} onChange={e => setUser({...user, name: e.target.value})}/>  
-                <label for="nome" class="labelInput">Nome:</label>
-                </div>
-                <br/><br/>
-                <div className='inputBox'>
-                <input placeholder='Digite seu email' type="email" name="email"  className="inputUser" value={user.email} onChange={e => setUser({...user, email: e.target.value})}/>
-                <label for="email"  class="labelInput">  E-mail: </label>
-                </div>
-                <br/><br/>
-                <div className='inputBox'>
-                <input id='text' placeholder='Digite uma senha' type="text" name="password"  className="inputUser" value={user.password} onChange={e => setUser({...user, password: e.target.value})}/> 
-                <label for="senha"  class="labelInput">  Senha: </label>
-                </div>
-                <br/><br/>
-                <button className="botao">Registrar Usuários</button>
-            </form>
-            </fieldset>
-            </div>
-    );
-  };
+          <div className='email'>
+          <input required placeholder='Digite seu email' type="email" name="email" value={user.email}
+          onChange={(e) => { setUser({ ...user, email: e.target.value }) }}/>
+          </div>
+
+          <div className='password'>
+          <input required placeholder='Digite uma senha' type="password" name="password" value={user.password}
+          onChange={(e) => { setUser({ ...user, password: e.target.value }) }}/>
+          </div>
+
+
+          <div className='submit'> 
+          <button className="botao" type='submit'>Registrar</button>
+          </div>   
+      </form>
+      <ToastContainer/>
+      </div>
+      </div>
+);
+}
