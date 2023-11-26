57eb4916f49f25444ced07f40d2f8cd60d9210c6
Vinícius <vinicius.solique@estudante.ifms.edu.br>
2023-11-26 15:41:19 -0400

    Upadade parte 2 no alterar

--- src/app/pages/dashboard/alter/page.js
+++ src/app/pages/dashboard/alter/page.js
@@ -1,48 +1,62 @@
 'use client'
 import { useState } from "react";
 import { useRouter } from 'next/navigation';
+import {ToastContainer, toast } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css'; 
 
-export default function Alterar() {
-    const [user, setUser] = useState({ name: '', email: '', password: ''})
+export default function AlterarUsers() {
     const route = useRouter();
-    const [name, setName] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
+    const [nameAlter, setnameAlter] = useState("");
+    const [passwordAlter, setpasswordAlter] = useState("");
+    const [emailAlter, setEmailAlter] = useState("");
+
 
     const alterar = (e) => {
-      e.preventDefault()
-      
-      const user = {
-        name: name,
-        password: password,
-        email: email
-      }
+        e.preventDefault()
 
+        const user = {
+            nameAlter: nameAlter,
+            passwordAlter: passwordAlter,
+            emailAlter: emailAlter
+        }
+        toast.success('Usuário alterado com sucesso!', {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: false,
+        });
     }
+return(
+    <div>
+    <h1 className='conta'>Alterar Usuário</h1>
+    <div className='container'>
+    <form className='contact_form' onSubmit={alterar}>
+        
+        <div className='nome'> 
+        <input required placeholder='Digite seu nome' type="text" name="name" value={nameAlter}
+        onChange={e => setnameAlter(e.target.value)}/>  
+        </div>
 
-    return (
-         <div className="conteiner">
-          <fieldset>
-            <legend><b>Alterar Usuários</b></legend>
-            <form onSubmit={alterar}>
-                <div className='inputBox'> 
-                <input id='text' placeholder='Digite seu Nome' type="text" name="name" className="inputUser" value={name} onChange={e => setName(e.target.value)}/>
-                <label for="nome" class="labelInput">Nome:</label>
-                </div>
-                <br/><br/>
-                <div className='inputBox'>
-                <input placeholder='Digite seu E-mail' type="email" name="email" className="inputUser" value={email} onChange={e => setEmail(e.target.value)}/>
-                <label for="email"  class="labelInput">  E-mail: </label>
-                </div>
-                <br/><br/>
-                <div className='inputBox'>
-                <input id='text' placeholder='Digite uma Senha' type="text" name="password"  className="inputUser" value={password} onChange={e => setPassword(e.target.value)}/>
-                <label for="senha"  class="labelInput">  Senha: </label>
-                </div>
-                <br/><br/>
-                <button className="botao">Alterar Usuários</button>
-            </form>
-            </fieldset>
-            </div>
-    );
+        <div className='email'>
+        <input required placeholder='Digite seu email' type="email" name="email" value={emailAlter}
+        onChange={e => setEmailAlter(e.target.value)}/>
+        </div>
+
+        <div className='password'>
+        <input required  placeholder='Digite uma nova senha' type="password" name="password" value={passwordAlter}
+        onChange={e => setpasswordAlter(e.target.value)}/> 
+        </div>
+
+
+        <div className='submit'> 
+        <button className="botao" type='submit'>Atualizar</button>
+        </div>  
+       
+    </form>
+    <ToastContainer/>
+    </div>
+    </div> 
+);
 }
