135fc072c751a12368cfc4953ca3a8e33213fd46
Vinícius <vinicius.solique@estudante.ifms.edu.br>
2023-11-28 14:39:47 -0400

    Update no falback

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -8,7 +8,7 @@ export default async function Dashboard() {
     return (
         <div>
             <h1>Usuários</h1>
-            <Suspense fallback={<p>Aguarde um instante. Carregando...</p>}>
+            <Suspense fallback={<p>Aguarde um instante. Estamos carregando as informações...</p>}>
                 <ListarUsuario users={users}/>
             </Suspense>
         </div>
